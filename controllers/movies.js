6ba6cfe8b6be120ad0befa4099b0c554894a5cf7
carrye <carryegaulin033178@gmail.com>
2020-11-15 01:26:34 -0500

    get all movies successful

--- controllers/movies.js
+++ controllers/movies.js
@@ -4,4 +4,31 @@ const getAllMovies = (request, response) => {
   return response.send(movies)
 }
 
-module.exports = { getAllMovies }
+const titleMatch = (request, response) => {
+  const { title } = request.params
+
+  const theTitle = movies.filter((movie) => movie.title === title)
+
+  return response.send(theTitle)
+}
+
+const saveNewMovie = (request, response) => {
+  const {
+    title, directors, releaseDate, rating, runTime, genres
+  } = request.body
+
+  if (!title || !directors || !releaseDate || !rating || !runTime || !genres) {
+    // eslint-disable-next-line max-len
+    return response.status(400).send('The following fields are required: location, mascot, abbreviation, conference, division')
+  }
+  const newMovie = {
+    // eslint-disable-next-line max-len
+    title: 'Cabin in the Woods', directors: '[Drew Goddard]', releaseDate: '04/13/2012', rating: 'R', runTime: '95 mins', genres: '[Horror]'
+  }
+
+  movies.push(newMovie)
+
+  return response.send(newMovie)
+}
+
+module.exports = { getAllMovies, titleMatch, saveNewMovie }
--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express')
-// const bodyParser = require('body-parser')
-const { getAllMovies } = require('./controllers/movies')
+const bodyParser = require('body-parser')
+const { getAllMovies, titleMatch, saveNewMovie } = require('./controllers/movies')
 const app = express()
 
-app.get('/movies', getAllMovies)
+app.get('/movies', getAllMovies, titleMatch)
+
+app.get('./movies/title:', titleMatch)
 
 
 app.all('*', (request, response) => {
   return response.sendStatus(404)
 })
 
-// app.post('/', bodyParser.json(), saveNewMovie) // obviously NOT saveNewHero
+app.post('/', bodyParser.json(), saveNewMovie)
 
 app.listen(1337, () => {
   // eslint-disable-next-line no-console
